d17d7ccecefe8138e4fbf7cb23058854c25e164f
Lucas Leblow <leblowl@gmail.com>
2014-02-16 15:59:58 -0800

    project.clj configured for cljs + om

---
+++ app.js
@@ -0,0 +1,3 @@
+goog.addDependency("base.js", ['goog'], []);
+goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.array', 'goog.object', 'goog.string.StringBuffer']);
+goog.addDependency("../photosure/core.js", ['photosure.core'], ['cljs.core']);
--- project.clj
+++ project.clj
@@ -3,4 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2156"]
+                 [om "0.4.2"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src"]
+                        :compiler {:output-to "app.js"
+                                   :output-dir "target"
+                                   :optimizations :none
+                                   :source-map true}}]})

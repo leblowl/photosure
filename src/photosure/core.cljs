ef8db16678d5f1a8a45e5c05959389c3a2c7facd
Max Chellemi <maxchellemi@Maxs-Mac.local>
2014-02-22 19:06:16 -0800

    gallery and photo components added

--- src/photosure/core.cljs
+++ src/photosure/core.cljs
@@ -2,20 +2,24 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
 
-(def app-state (atom {:list ["Lion" "Zebra"]}))
+(def app-state (atom {:list ["I", "LOVE", "YOU"]}))
 
-
-(defn gallery [state owner]
+(defn photo [title owner]
   (reify
     om/IRender
     (render [this]
-      (dom/div #js {:id "backdrop"}
-               (dom/div nil "Beginning")
-               (dom/div nil "Middle")
-               (dom/div nil "End")))))
+      (dom/div nil title))))
+
+(defn gallery [titles owner]
+  (reify
+    om/IRender
+    (render [this]
+      (apply dom/div
+             nil
+             (om/build-all photo (:list titles))))))
 
 
-(defn run [app]
+(defn run [app app-state]
   (om/root app app-state {:target (. js/document (getElementById "app"))}))
 
-(run gallery)
+(run gallery app-state)

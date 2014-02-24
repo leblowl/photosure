14f9fac2e99c29270f32cf9d53672dfc691ef0d1
Lucas Leblow <leblowl@gmail.com>
2014-02-23 17:03:37 -0800

    add next btn and core async import

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2156"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [om "0.4.2"]]
   :plugins [[lein-cljsbuild "1.0.2"]]
   :cljsbuild {:builds [{:id "dev"
--- src/photosure/core.cljs
+++ src/photosure/core.cljs
@@ -1,23 +1,31 @@
 (ns photosure.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <!]]))
 
-(def app-state (atom {:list ["I", "LOVE", "YOU"]}))
+(def app-state (atom {:photos ["I" "LOVE" "YOU"]}))
 
-(defn photo [title owner]
+(defn photo [photo owner]
   (reify
     om/IRender
     (render [this]
-      (dom/div nil title))))
+      (dom/div nil photo))))
 
-(defn gallery [titles owner]
+(defn next-btn [app-state owner]
+  (reify
+    om/IRenderState
+    (render-state [this {:keys [next]}]
+      (dom/button #js {:id "next-btn" :onClick (fn [e] (put! next "You asked for it!"))} "Next"))))
+
+(defn gallery [app-state owner]
   (reify
     om/IRender
     (render [this]
-      (apply dom/div
-             nil
-             (om/build-all photo (:list titles))))))
-
+      (dom/div #js {:id "photo-gallery-container"}
+        (apply dom/div #js {:id "photo-gallery"}
+          (om/build-all photo (:photos app-state)))
+         (om/build next-btn app-state)))))
 
 (defn run [app app-state]
   (om/root app app-state {:target (. js/document (getElementById "app"))}))

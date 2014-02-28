525111c724269733c9f5003fbbc7e66384c1a6cd
Lucas Leblow <leblowl@gmail.com>
2014-02-27 21:39:04 -0800

    weird bug with prepending vector in om, where translate3d doesn't kick in for a single render

--- src/photosure/core.cljs
+++ src/photosure/core.cljs
@@ -5,17 +5,39 @@
             [cljs.core.async :refer [put! chan <! timeout]]))
 
 (def photos ["I" "LOVE" "YOU"])
-(def index (atom 0))
 
-(def app-state (atom {:photos [{:photo "rsc/flower1.jpg" :state "center"} {:photo "rsc/puppy1.jpg" :state "enter"}]
-                      :index 0}))
+(def next-photos [1 2 3])
+(def prev-photos [])
 
-(defn inc-index [app-state]
-  (swap! app-state assoc :index (inc (:index @app-state))))
+(def app-state (atom {:next-photos [{:photo "rsc/cpleblow2.jpg" :state "enter"}
+                                    {:photo "rsc/cpleblow3.jpg" :state "enter"}]
 
-(defn get-next-photo [app-state]
-  (inc-index app-state)
-  (get (:photos @app-state) (:index @app-state)))
+                      :current-photos [{:photo "rsc/cpleblow1.jpg" :state "enter"}
+                                       {:photo "rsc/cpleblow2.jpg" :state "center"}
+                                       {:photo "rsc/cpleblow3.jpg" :state "leave"}]
+
+                      :prev-photos [{:photo "2" :state "2"}]}))
+
+(defn next-photo [app-state]
+  (om/transact! app-state
+    (fn [_]
+      {:prev-photos (conj (:prev-photos _) (peek (:next-photos _)))
+       :next-photos (pop (:next-photos _))})))
+
+(defn transition-next [app-state]
+  (om/transact! app-state :current-photo
+    (fn [_]
+      (assoc _ :state "center"))))
+
+(defn new-next [app-state]
+  (om/update! app-state assoc-in [:current-photos 1 :state] "leave")
+  (om/update! app-state assoc-in [:current-photos 0 :state] "center"))
+
+(defn prev-photo [app-state]
+  (om/transact! app-state
+    (fn [_]
+      {:next-photos (conj (:next-photos _) (peek (:prev-photos _)))
+       :prev-photos (pop (:prev-photos _))})))
 
 (defn photo [photo owner]
   (reify
@@ -27,7 +49,7 @@
   (reify
     om/IRenderState
     (render-state [this {:keys [next]}]
-      (dom/div #js {:id "prev-btn" :className "btn" :onClick (fn [e] (put! next "flower1.jpg"))}))))
+      (dom/div #js {:id "prev-btn" :className "btn" :onClick (fn [e] (put! next "cpleblow1.jpg"))}))))
 
 (defn next-btn [app-state owner]
   (reify
@@ -47,15 +69,28 @@
       (let [next (om/get-state owner :next)]
         (go (loop []
               (let [photo (<! next)]
-                (om/transact! app-state :photos
-                  (fn [xs]
-                    (assoc-in xs [0 :state] "leave")
-                    (assoc-in xs [1 :state] "center")))
+
+                (om/transact! app-state [:current-photos 2 :state]
+                  (fn [_] "enter"))
+                (om/transact! app-state [:current-photos 1 :state]
+                  (fn [_] "slide leave"))
+                (om/transact! app-state [:current-photos 0 :state]
+                  (fn [_] "slide center"))
+
                 (<! (timeout 1500))
-                (om/transact! app-state :photos
-                  (fn [xs]
-                    [] ;pop first item off
-                    (assoc-in xs [(count xs)] {:photo "rsc/puppy1.jpg" :state "enter"}))) ;push to tail
+                (om/transact! app-state [:current-photos 1 :state]
+                  (fn [_] "leave"))
+                (om/transact! app-state [:current-photos 0 :state]
+                  (fn [_] "center"))
+
+                (<! (timeout 500))
+                (om/transact! app-state :current-photos
+                  (fn [_] (pop _)))
+
+                (<! (timeout 500))
+                (om/transact! app-state :current-photos
+                  (fn [_] (into (pop (:next-photos @app-state)) _)))
+
                 (recur))))))
 
     om/IRenderState
@@ -63,7 +98,7 @@
       (dom/div #js {:id "photo-gallery-container"}
         (dom/div #js {:id "left-pane"} (om/build prev-btn app-state {:init-state {:next next}}))
         (apply dom/div #js {:id "photo-gallery"}
-          (om/build-all photo (:photos app-state)))
+          (om/build-all photo (:current-photos app-state)))
         (dom/div #js {:id "right-pane"} (om/build next-btn app-state {:init-state {:next next}}))))))
 
 (defn run [app app-state]

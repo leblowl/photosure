4cfb28ffd18020306f45c591a200180e6a1109e5
Lucas Leblow <leblowl@gmail.com>
2014-03-12 21:09:42 -0700

    gallery glitch bug worked out, sliding images working

--- src/photosure/client/gallery.cljs
+++ src/photosure/client/gallery.cljs
@@ -8,16 +8,23 @@
 
 (def app-state
   (atom
-   {:current-photos
-    [{:photo "images/cpleblow1.jpg" :state "atleft"}
-     {:photo "images/cpleblow2.jpg" :state "atcenter"}
-     {:photo "images/cpleblow3.jpg" :state "atright"}]}))
+   {:next-photos    [{:photo "images/cpleblow3.jpg" :state "atleft"}]
+
+    :current-photos [{:photo "images/cpleblow1.jpg" :state "atleft"}
+                     {:photo "images/cpleblow2.jpg" :state "atcenter"}
+                     {:photo "images/cpleblow3.jpg" :state "atright"}]
+
+    :prev-photos    [{:photo "images/cpleblow1.jpg" :state "atright"}]}))
 
 (defn photo [photo owner]
   (reify
     om/IRender
     (render [this]
         (dom/img #js {:src (:photo photo) :id (:id photo) :className (str "photo " (:state photo))}))))
+
+(defn slide-next [app]
+  (om/transact! app [:current-photos 1 :state] (fn [_] (str _ " transition atright")))
+  (om/transact! app [:current-photos 0 :state] (fn [_] (str _ " transition atcenter"))))
 
 (defn prev-btn [app owner]
   (reify
@@ -42,20 +49,25 @@
       (let [next-photo (om/get-state owner :next-chan)]
         (go (loop []
               (let [photo (<! next-photo)]
+                (om/transact! app [:current-photos 0 :state] (fn [_] (str _ " transition atcenter")))
+                (om/transact! app [:current-photos 1 :state] (fn [_] (str _ " transition atright")))
+                (<! (timeout 500))
+                (om/update! app [:current-photos 0 :state] "atcenter")
+                (om/update! app [:current-photos 1 :state] "atright")
 
-                (.log js/console "IWillMount: updating app state")
-                (om/transact! app :current-photos (fn [xs] (subvec xs 1)))
+
+                (om/transact! app :current-photos (fn [xs] (into [{:photo "images/cpleblow3.jpg" :state "atleft"}] (pop xs))))
+
+                ;(slide-next app)
+                ;(om/transact! app :current-photos (fn [xs] (subvec xs 1)))
                 (recur))))))
 
-    om/IWillUpdate
-    (will-update [_ next-props next-state]
-      (.log js/console "IWillUpdate: ready to update!"))
     om/IRenderState
     (render-state [this {:keys [next-chan]}]
       (dom/div #js {:id "photo-gallery-container"}
         (dom/div #js {:id "left-pane"} (om/build prev-btn app {:init-state {:next next-chan}}))
         (apply dom/div #js {:id "photo-gallery"}
-          (om/build-all photo (:current-photos app)))
+          (om/build-all photo (:current-photos app) {:key :photo}))
         (dom/div #js {:id "right-pane"} (om/build next-btn app {:init-state {:next next-chan}}))))))
 
 (defn run [] (om/root gallery app-state {:target (. js/document (getElementById "gallery"))}))
--- src/photosure/server/core.cljs
+++ src/photosure/server/core.cljs
@@ -9,6 +9,18 @@
       (.send response response-body)
       (next))))
 
+(defn fetch-posts [request response next]
+  (let [tumblr (nodejs/require "tumblr.js")
+        client (.createClient tumblr cljs/clj->js {:consumer_key "nArQ3AMh7kkiU72PjowZxx1eYZ0PxIFpHFojpxtsoqnjvEJ6CC"
+                                                   :consumer_secret "f3UwUKOEugWVbszKWUDfH4r2KcqP8hf67tzv0X5H7HVSAWrAig"
+                                                   :token "fdlkjsa"
+                                                   :token_secret "fdkljsalfds"})
+        response-body (cljs/clj->js {:eh "maybe?"})]
+    (do
+      (.userInfo client (fn [err data] (.log js/console err) (.log js/console (.-blogs data))))
+      (.send response response-body)
+      (next))))
+
 (defn create-server []
   (let [restify (nodejs/require "restify")
         server (.createServer restify)
@@ -17,6 +29,7 @@
         static-file-server (.serveStatic restify static-server-opts)]
     (do
       (.get server "/greeting/:name" say-hello)
+      (.get server "/tumblr" fetch-posts)
       (.get server static-file-regexp static-file-server))
     server))
 

8a8fa500f2e6160c7b5560ee06b8fcd7212099b1
Lucas Leblow <leblowl@gmail.com>
2014-06-03 21:05:28 -0700

    moved html to public/html and http-kit now server main html

--- src/photosure/server/server.clj
+++ src/photosure/server/server.clj
@@ -4,6 +4,7 @@
   (:use [compojure.route :only [files not-found resources]]
         [compojure.handler :only [site]]
         [compojure.core :only [defroutes GET POST DELETE ANY context]]
+        [ring.util.response :only [file-response]]
         [net.cgrand.enlive-html]))
 
 (def data {:blog_name "cpleblow"
@@ -15,7 +16,7 @@
 (def api "http://api.tumblr.com/v2/blog/cpleblow.tumblr.com/posts?api_key=Tg1KPogMJGHntn64LnRYkTK0pAdlt5VdihWiDNEuNjrYH7TB20&limit=2")
 (def posts (get-in (client/get api {:as :json}) [:body :response :posts]))
 
-(defsnippet post-list-item "public/post.html"
+(defsnippet post-list-item "public/html/post.html"
   [:.post]
   [id image caption]
 
@@ -23,18 +24,21 @@
   [:div :div.photo :img] (set-attr :src image)
   [:div :div.caption] (html-content caption))
 
-(deftemplate post-list "public/post.html" [posts]
+(deftemplate post-list "public/html/post.html" [posts]
   [:body] (content (map (fn [post] (post-list-item (:id post)
                                                   (get-in post [:photos 0 :alt_sizes 0 :url])
                                                   (:caption post)))
                         posts)))
 
-(defn show-blog [req]
-  ;;enlive here
+(defn app [req]
+  (file-response "public/html/photosure.html" {:root "resources"}))
+
+(defn blog [req]
   (apply str (post-list posts)))
 
 (defroutes all-routes
-  (GET "/blog" [] show-blog)
+  (GET "/" [] app)
+  (GET "/blog" [] blog)
   (resources "/")
   (not-found "<p>Page not found.</p>"))
 
@@ -53,4 +57,4 @@
   (start))
 
 (defn -main [&args]
-  (start-server))
+  (start))

4d86765955679ef87c7d6ec845115073bfe4fbea
Lucas Leblow <leblowl@gmail.com>
2014-07-30 12:45:47 -0700

    util class for edn added

---
+++ src/photosure/client/util.cljs
@@ -0,0 +1,20 @@
+(ns photosure.client.util
+  (:require [cljs.reader :as reader]
+            [goog.events :as events])
+  (:import [goog.net XhrIo]
+           [goog.events EventType]))
+
+(def ^:private meths
+  {:get "GET"
+   :put "PUT"
+   :post "POST"
+   :delete "DELETE"})
+
+(defn edn-xhr [{:keys [method url data on-complete]}]
+  (let [xhr (XhrIo.)]
+    (events/listen xhr EventType.COMPLETE
+                   (fn [e]
+                     (on-complete (reader/read-string (.getResponseText xhr)))))
+    (. xhr
+       (send url (meths method) (when data (pr-str data))
+             #js {"Content-Type" "application/edn"}))))

54514889e911fe17aa90026636f02727ca50f9b8
Lucas Leblow <leblowl@gmail.com>
2014-08-01 13:20:34 -0700

    using transition with transform3d now for animations, simplified, smoother

--- src/photosure/client/gallery.cljs
+++ src/photosure/client/gallery.cljs
@@ -7,9 +7,6 @@
 
 "I LOVE YOU"
 
-(defn filter-indexed [f coll]
-  (map second (filter (fn [[ndx item]] (f ndx item)) (map vector (range) coll))))
-
 (defn photo [filepath pos] {:photo filepath :pos pos})
 
 (def app-state
@@ -28,7 +25,7 @@
     (render [this]
       (dom/div #js {:className (str "frame " (if (empty? (:pos photo))
                                                 "hidden"
-                                                (str/join "-" (take-last 2 (:pos photo)))))}
+                                                (get (:pos photo) 0)))}
        (dom/img #js {:src (:photo photo)
                      :className "photo"})))))
 
@@ -36,22 +33,17 @@
   (reify
     om/IRenderState
     (render-state [this {:keys [slide-chan disabled]}]
-      (dom/div #js {:id "prev-btn" :className (str "btn" (when disabled " disabled")) :onClick (fn [e] (put! slide-chan "prev"))}))))
+      (dom/div #js {:id "prev-btn"
+                    :className (str "btn" (when disabled " disabled"))
+                    :onClick (fn [e] (put! slide-chan "prev"))}))))
 
 (defn next-btn [app owner]
   (reify
     om/IRenderState
     (render-state [this {:keys [slide-chan disabled]}]
-      (dom/div #js {:id "next-btn" :className (str "btn" (when disabled " disabled")) :onClick (fn [e] (put! slide-chan "next"))}))))
-
-(defn originate [app ndx from]
-  (om/update! app [:photos ndx :pos] [from]))
-
-(defn transition [app ndx to]
-  (om/transact! app [:photos ndx :pos] #(conj % to)))
-
-(defn disappear [app ndx]
-  (om/update! app [:photos ndx :pos] []))
+      (dom/div #js {:id "next-btn"
+                    :className (str "btn" (when disabled " disabled"))
+                    :onClick (fn [e] (put! slide-chan "next"))}))))
 
 (defn gallery [app owner]
   (reify
@@ -64,30 +56,23 @@
     (will-mount [_]
       (let [slide-chan (om/get-state owner :slide-chan)]
         (go (loop []
-              (let [len (count (:photos @app))]
-                (if (= (<! slide-chan) "next")
-                  (do
-                    (disappear app (get (:curr @app) 2))
-                    (om/transact! app :curr (fn [_] (apply vector (map #(mod (dec %) len) _))))
-                    (om/set-state! owner :anim-in-progress true)
-                    (originate  app (get (:curr @app) 0) "left")
-                    (transition app (get (:curr @app) 1) "center")
-                    (transition app (get (:curr @app) 2) "right")
-                    (<! (timeout 1000))
-                    (om/set-state! owner :anim-in-progress false)
-                    (originate app (get (:curr @app) 1) "center")
-                    (originate app (get (:curr @app) 2) "right"))
-                  (do
-                    (disappear app (get (:curr @app) 0))
-                    (om/transact! app :curr (fn [_] (apply vector (map #(mod (inc %) len) _))))
-                    (om/set-state! owner :anim-in-progress true)
-                    (transition app (get (:curr @app) 0) "left")
-                    (transition app (get (:curr @app) 1) "center")
-                    (originate  app (get (:curr @app) 2) "right")
-                    (<! (timeout 1000))
-                    (om/set-state! owner :anim-in-progress false)
-                    (originate app (get (:curr @app) 0) "left")
-                    (originate app (get (:curr @app) 1) "center")))
+              (let [len (count (:photos @app))
+                    cmd (<! slide-chan)]
+                (do
+                  (if (= cmd "next")
+                    (do (om/update! app [:photos (get (:curr @app) 2) :pos] [])
+                        (om/transact! app :curr
+                                      (fn [_] (apply vector (map #(mod (dec %) len) _)))))
+                    (do  (om/update! app [:photos (get (:curr @app) 0) :pos] [])
+                         (om/transact! app :curr
+                                       (fn [_] (apply vector (map #(mod (inc %) len) _))))))
+
+                  (om/set-state! owner :anim-in-progress true)
+                  (om/update! app [:photos (get (:curr @app) 0) :pos] ["left"])
+                  (om/update! app [:photos (get (:curr @app) 1) :pos] ["center"])
+                  (om/update! app [:photos (get (:curr @app) 2) :pos] ["right"])
+                  (<! (timeout 1000))
+                  (om/set-state! owner :anim-in-progress false))
                 (recur))))))
 
     om/IRenderState
@@ -102,4 +87,7 @@
           (om/build next-btn app {:init-state {:slide-chan slide-chan}
                                   :state {:disabled anim-in-progress}}))))))
 
-(defn render [] (om/root gallery app-state {:target (. js/document (getElementById "dynamic-content"))}))
+(defn render []
+  (om/root gallery
+           app-state
+           {:target (. js/document (getElementById "dynamic-content"))}))
